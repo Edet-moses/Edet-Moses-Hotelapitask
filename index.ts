9f4aa20ff6187f79703bebd71808e92d0983bb9c
Couragenwanduka <156565155+Couragenwanduka@users.noreply.github.com>
2024-04-01 14:01:47 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -15,9 +15,7 @@ app.use(errorHandler);
 app.use('/', router);
 
 const PORT = process.env.PORT || 5000;
-app.get('/',(req,res)=>{
-  res.send('bonjour')
-})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
